f5291b4f99186cedbf5583a48fa4e4cd010973e1
John Pearson <jmxpearson@gmail.com>
2016-06-30 09:35:50 -0400

    example changes for code review

--- src/univariate/continuous/triangular.jl
+++ src/univariate/continuous/triangular.jl
@@ -41,16 +41,16 @@ immutable TriangularDist{T <: Real} <: ContinuousUnivariateDistribution
     end
 end
 
-TriangularDist{T <: Real}(a::T, b::T, c::T) = TriangularDist{T}(a, b, c)
+TriangularDist{T<:Real}(a::T, b::T, c::T) = TriangularDist{T}(a, b, c)
 TriangularDist(a::Real, b::Real, c::Real) = TriangularDist(promote(a, b, c)...)
-function TriangularDist(a::Integer, b::Integer, c::Integer)
-    TriangularDist(Float64(a), Float64(b), Float64(c))
-end
-TriangularDist{T <: Real}(a::T, b::T) = TriangularDist{T}(a, b)
+# function TriangularDist(a::Integer, b::Integer, c::Integer)
+#     TriangularDist(Float64(a), Float64(b), Float64(c))
+# end
+TriangularDist{T<:Real}(a::T, b::T) = TriangularDist{T}(a, b)
 TriangularDist(a::Real, b::Real) = TriangularDist(promote(a, b)...)
-function TriangularDist(a::Integer, b::Integer)
-    TriangularDist(Float64(a), Float64(b))
-end
+# function TriangularDist(a::Integer, b::Integer)
+#     TriangularDist(Float64(a), Float64(b))
+# end
 
 @distr_support TriangularDist d.a d.b
 
@@ -69,35 +69,35 @@ mean(d::TriangularDist) = (d.a + d.b + d.c) / 3.0
 function median(d::TriangularDist)
     (a, b, c) = params(d)
     m = middle(a, b)
-    c >= m ? a + sqrt(0.5 * (b - a) * (c - a)) :
-             b - sqrt(0.5 * (b - a) * (b - c))
+    c >= m ? a + sqrt((b - a) * (c - a)/2) :
+             b - sqrt((b - a) * (b - c)/2)
 end
 
-_pretvar(a::Float64, b::Float64, c::Float64) = a*a + b*b + c*c - a*b - a*c - b*c
+_pretvar(a::Real, b::Real, c::Real) = a*a + b*b + c*c - a*b - a*c - b*c
 
 function var(d::TriangularDist)
     (a, b, c) = params(d)
-    _pretvar(a, b, c) / 18.0
+    _pretvar(a, b, c) / 18
 end
 
 function skewness(d::TriangularDist)
     (a, b, c) = params(d)
-    sqrt2 * (a + b - 2.0c) * (2.0a - b - c) * (a - 2.0b + c) / (5.0 * _pretvar(a, b, c)^1.5)
+    sqrt2 * (a + b - 2c) * (2a - b - c) * (a - 2b + c) / (5 * _pretvar(a, b, c)^3//2)
 end
 
-kurtosis{T <: Real}(d::TriangularDist{T}) = -0.6*one(T)
+kurtosis{T<:Real}(d::TriangularDist{T}) = -0.6 * one(T)
 
-entropy(d::TriangularDist) = 0.5 + log((d.b - d.a) / 2.0)
+entropy(d::TriangularDist) = 1//2 + log((d.b - d.a) / 2)
 
 
 #### Evaluation
 
-function pdf{T <: Real}(d::TriangularDist{T}, x::Real)
+function pdf{T<:Real}(d::TriangularDist{T}, x::Real)
     (a, b, c) = params(d)
     x <= a ? zero(T) :
-    x <  c ? 2.0 * (x - a) / ((b - a) * (c - a)) :
-    x == c ? 2.0 / (b - a) :
-    x <= b ? 2.0 * (b - x) / ((b - a) * (b - c)) : zero(T)
+    x <  c ? 2 * (x - a) / ((b - a) * (c - a)) :
+    x == c ? 2 / (b - a) :
+    x <= b ? 2 * (b - x) / ((b - a) * (b - c)) : zero(T)
 end
 
 function cdf{T <: Real}(d::TriangularDist{T}, x::Real)
@@ -105,7 +105,7 @@ function cdf{T <: Real}(d::TriangularDist{T}, x::Real)
     x <= a ? zero(T) :
     x <  c ? (x - a)^2 / ((b - a) * (c - a)) :
     x == c ? (c - a) / (b - a) :
-    x <= b ? 1.0 - (b - x)^2 / ((b - a) * (b - c)) : one(T)
+    x <= b ? 1 - (b - x)^2 / ((b - a) * (b - c)) : one(T)
 end
 
 function quantile(d::TriangularDist, p::Real)
@@ -114,7 +114,7 @@ function quantile(d::TriangularDist, p::Real)
     b_m_a = b - a
     rl = c_m_a / b_m_a
     p <= rl ? a + sqrt(b_m_a * c_m_a * p) :
-              b - sqrt(b_m_a * (b - c) * (1.0 - p))
+              b - sqrt(b_m_a * (b - c) * (1 - p))
 end
 
 function mgf{T <: Real}(d::TriangularDist{T}, t::Real)
@@ -124,19 +124,19 @@ function mgf{T <: Real}(d::TriangularDist{T}, t::Real)
         (a, b, c) = params(d)
         u = (b - c) * exp(a * t) - (b - a) * exp(c * t) + (c - a) * exp(b * t)
         v = (b - a) * (c - a) * (b - c) * t^2
-        return 2.0 * u / v
+        return 2u / v
     end
 end
 
 function cf{T <: Real}(d::TriangularDist{T}, t::Real)
     # Is this correct?
     if t == zero(t)
-        return one(T)
+        return Complex{one(T)}
     else
         (a, b, c) = params(d)
         u = (b - c) * cis(a * t) - (b - a) * cis(c * t) + (c - a) * cis(b * t)
         v = (b - a) * (c - a) * (b - c) * t^2
-        return -2.0 * u / v
+        return -2u / v
     end
 end
 
@@ -148,5 +148,5 @@ function rand(d::TriangularDist)
     b_m_a = b - a
     u = rand()
     b_m_a * u < (c - a) ? d.a + sqrt(u * b_m_a * (c - a)) :
-                          d.b - sqrt((1.0 - u) * b_m_a * (b - c))
+                          d.b - sqrt((1 - u) * b_m_a * (b - c))
 end
